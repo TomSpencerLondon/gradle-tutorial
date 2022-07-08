bbd869330fb78fe59b9f083ed9316134d66de27a
Tom <thomas.spencer@codurance.com>
2022-07-08 08:37:04 +0100

    Add practice code for gradle commands

---
+++ my-webapp/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
@@ -0,0 +1,25 @@
+package com.example.multimodule.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.multimodule.service.MyService;
+
+@SpringBootTest
+public class DemoApplicationTest {
+
+	@Autowired
+	private MyService myService;
+
+	@Test
+	public void contextLoads() {
+		ImmutableSet.of(1, 2, 3);
+		assertThat(myService.message() + "meh").isNotNull();
+	}
+
+}
